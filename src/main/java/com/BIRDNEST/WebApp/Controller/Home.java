9661a92c20053a9c1bbec2f7ee65872c263379f6
itkimura <itkimura@student.hive.fi>
2023-01-01 23:31:45 +0200

    loop drone to add

--- src/main/java/com/BIRDNEST/WebApp/Controller/Home.java
+++ src/main/java/com/BIRDNEST/WebApp/Controller/Home.java
@@ -1,6 +1,7 @@
 package com.BIRDNEST.WebApp.Controller;
 
 import com.BIRDNEST.WebApp.BirdnestClient;
+import com.BIRDNEST.WebApp.Data.Drone;
 import com.BIRDNEST.WebApp.Data.Violator;
 import com.BIRDNEST.WebApp.Monitor;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,7 +28,23 @@ public class Home {
         var report = client.getReport();
         //var pilot = sut.getPilot(actual.capture().drones()[0].serialNumber());
         var drones = report.capture().drones();
-
+        for (Drone drone: drones) {
+            if (monitor.IsDroneInNDZ(drone.positionX(), drone.positionY()))
+            {
+                Violator violator;
+                if (violators.containsKey(drone.serialNumber())) {
+                    violator = violators.get(drone.serialNumber());
+                    violator.UpdateDrone(drone);
+                    System.out.println("Update:" + violator.pilot().firstName());
+                }
+                else
+                {
+                    var pilot = client.getPilot(drone.serialNumber());
+                    violator = new Violator(pilot, drone, report.capture());
+                    violators.put(drone.serialNumber(), violator);
+                }
+            }
+        }
         System.out.println("---------");
         for (Map.Entry<String, Violator> entry: violators.entrySet()) {
             System.out.print("[" + entry.getValue().pilot().firstName() + " ");
