0ab6e766e744a9df13e4658ae76f21fa5b4b584e
itkimura <itkimura@student.hive.fi>
2023-01-12 12:17:13 +0200

    Clean up tests

--- src/test/java/com/birdnest/application/MonitorTests.java
+++ src/test/java/com/birdnest/application/MonitorTests.java
@@ -20,6 +20,18 @@ import static org.mockito.Mockito.*;
 @SpringBootTest
 public class MonitorTests {
 
+    Drone[] createTestDroneArray(){
+        Drone drone = new Drone("Test1234", "Test", "Test", "Test",
+                "Test", "Test", "Test", 250000.00f, 250000.00f, 5000.0f);
+        Drone[] drones = new Drone[1];
+        drones[0] = drone;
+        return drones;
+    }
+
+    Pilot createTestPilot(){
+        Pilot pilot = new Pilot("Test", "Test", "Test", "Test", "Test", "Test");
+        return pilot;
+    }
     @Test
     void CalculateDistance_WhenDistanceZero_Zero(){
         var sut = new Monitor(mock(BirdnestClient.class));
@@ -34,79 +46,69 @@ public class MonitorTests {
         assertTrue((100000 == distance), "Should return 100m");
     }
 
-    Drone[] createTestDroneArray(){
-        Drone drone = new Drone("Test1234", "Test", "Test", "Test",
-                "Test", "Test", "Test", 250000.00f, 250000.00f, 5000.0f);
-        Drone[] drones = new Drone[1];
-        drones[0] = drone;
-    }
     @Test
     void AddNewViolator_WhenCannotGetPilot_SetPilotNotAvailable() throws Exception {
-        Drone drone = new Drone("Test1234", "Test", "Test", "Test",
-                "Test", "Test", "Test", 250000.00f, 250000.00f, 5000.0f);
-        Drone[] drones = new Drone[1];
-        drones[0] = drone;
+        var drones = createTestDroneArray();
         Capture capture = new Capture(drones, LocalDateTime.now(ZoneId.of("UTC")));
-        double distance = 0;
         var mockedClient = mock(BirdnestClient.class);
         var sut = new Monitor(mockedClient);
-        sut.addNewViolator(drone, capture, distance);
+        sut.addNewViolator(drones[0], capture, 0);
         var actual = sut.getViolatorReport().violators();
         assertTrue(actual.length == 1 && actual[0].getPilot().pilotId() == "Not Available", "Pilot should be not available");
     }
 
     @Test
     void AddNewViolator_WhenGetPilot_AddPilot() throws Exception {
-        Drone drone = new Drone("Test1234", "Test", "Test", "Test",
-                "Test", "Test", "Test", 250000.00f, 250000.00f, 5000.0f);
-        Pilot pilot = new Pilot("Test", "Test", "Test", "Test", "Test", "Test");
-        Drone[] drones = new Drone[1];
-        drones[0] = drone;
+        var drones = createTestDroneArray();
+        var pilot = createTestPilot();
         Capture capture = new Capture(drones, LocalDateTime.now(ZoneId.of("UTC")));
         var mockedClient = mock(BirdnestClient.class);
         when(mockedClient.getPilot(anyString())).thenReturn(pilot);
         var sut = new Monitor(mockedClient);
-        sut.addNewViolator(drone, capture, 0);
+        sut.addNewViolator(drones[0], capture, 0);
         var actual = sut.getViolatorReport().violators();
         assertTrue(actual.length == 1 && actual[0].getPilot() == pilot, "Pilot should be the test pilot");
     }
 
     @Test
     void UpdateViolator_WhenViolatorSeenAgain_UpdateTime() throws Exception {
-        Drone drone = new Drone("Test1234", "Test", "Test", "Test",
-                "Test", "Test", "Test", 250000.00f, 250000.00f, 5000.0f);
-        Pilot pilot = new Pilot("Test", "Test", "Test", "Test", "Test", "Test");
-        Drone[] drones = new Drone[1];
-        drones[0] = drone;
+        var drones = createTestDroneArray();
+        var pilot = createTestPilot();
         Capture capture = new Capture(drones, LocalDateTime.now(ZoneId.of("UTC")).plusMinutes(-5));
+
         var mockedClient = mock(BirdnestClient.class);
         when(mockedClient.getPilot(anyString())).thenReturn(pilot);
+
         var sut = new Monitor(mockedClient);
-        sut.addNewViolator(drone, capture, 0);
+        sut.addNewViolator(drones[0], capture, 0);
+        sut.printViolators();
+
         var time = LocalDateTime.now(ZoneId.of("UTC"));
         capture = new Capture(drones, time);
-        sut.updateViolator(drone, capture, 0);
+        sut.updateViolator(drones[0], capture, 0);
         var actual = sut.getViolatorReport().violators()[0].getTime();
+        sut.printViolators();
+
         assertTrue(time == actual, "Time should be updated");
     }
 
     @Test
     void UpdateViolator_WhenViolatorSeenAgain_UpdateTimeAndDistance() throws Exception {
-        Drone drone = new Drone("Test1234", "Test", "Test", "Test",
-                "Test", "Test", "Test", 250000.00f, 250000.00f, 5000.0f);
-        Pilot pilot = new Pilot("Test", "Test", "Test", "Test", "Test", "Test");
-        Drone[] drones = new Drone[1];
-        drones[0] = drone;
+        var drones = createTestDroneArray();
+        var pilot = createTestPilot();
         Capture capture = new Capture(drones, LocalDateTime.now(ZoneId.of("UTC")).plusMinutes(-5));
+
         var mockedClient = mock(BirdnestClient.class);
         when(mockedClient.getPilot(anyString())).thenReturn(pilot);
+
         var sut = new Monitor(mockedClient);
-        sut.addNewViolator(drone, capture, 90000);
+        sut.addNewViolator(drones[0], capture, 90000);
         var actual = sut.getViolatorReport().violators()[0].getDistance();
         sut.printViolators();
+
         var distance = 50000;
         capture = new Capture(drones, LocalDateTime.now(ZoneId.of("UTC")));
-        sut.updateViolator(drone, capture, distance);
+        sut.updateViolator(drones[0], capture, distance);
         actual = sut.getViolatorReport().violators()[0].getDistance();
         sut.printViolators();
         assertTrue(distance == actual, "Time should be updated");
@@ -115,18 +117,18 @@ public class MonitorTests {
 
     @Test
     void RemoveExpired_WhenTimeIsOverTen_RemoveViolator(){
-        Drone drone = new Drone("Test1234", "Test", "Test", "Test",
-                "Test", "Test", "Test", 250000.00f, 250000.00f, 5000.0f);
-        Drone[] drones = new Drone[1];
-        drones[0] = drone;
-        Pilot pilot = new Pilot("Test", "Test", "Test", "Test", "Test", "Test");
+        var drones = createTestDroneArray();
+        var pilot = createTestPilot();
         Capture capture = new Capture(drones, LocalDateTime.now(ZoneId.of("UTC")).plusMinutes(-11));
-        var violator = new Violator(pilot, drone, capture, 0);
+
+        var violator = new Violator(pilot, drones[0], capture, 0);
         ConcurrentMap<String, Violator> violators = new ConcurrentHashMap<>();
         violators.put("Test", violator);
         var mockedClient = mock(BirdnestClient.class);
         var sut = new Monitor(mockedClient);
+        System.out.println("Before:" + violators);
         sut.removeExpired(violators);
+        System.out.println("After:" + violators);
         assertTrue(violators.size() == 0, "Violators should be removed");
 
     }
